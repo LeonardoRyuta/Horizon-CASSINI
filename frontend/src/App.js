70974943905019b3cd09f5250f8828c9205860a6
nourchabchoub <n.chabchoub@student.utwente.nl>
2024-11-23 16:25:37 +0100

    added tailwind and daisyui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2 className="h2">
-          Upload Your Data
-        </h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body >
+      <div className="App">
+        <header className="App-header">
+          <h2 className="h2">
+            Upload Your Data
+          </h2>
+          <div className="card bg-base-100 w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </body>
   );
 }
 
